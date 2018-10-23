aa6bcadd66cd573929f6b4527a342df113d5289a
Daniel Jílek <jilek.daniel@gmail.com>
2018-10-23 21:10:17 +0200

    update jira edit dialog script

--- jiraLargerEditDialog.js
+++ jiraLargerEditDialog.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Creates larger edit dialog in JIRA
-// @author       Daniel Jílek
+// @author       You
 // @match        https://jira.unicorn.eu/browse/*
 // @grant        none
 // ==/UserScript==
@@ -14,22 +14,17 @@
     var observer = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
             if (!mutation.addedNodes) return
-               for (var i = 0; i < mutation.addedNodes.length; i++) {
-                   var editComment = document.getElementById("edit-comment");
-                   editComment.style.width = '80%';
-                   editComment.style.marginLeft = '0';
-                   editComment.style.marginRight = '0';
-                   editComment.style.left = '10%';
-                   editComment.style.top = '10%';
-                   editComment.style.marginTop = '0';
-                   editComment.style.position = 'fixed';
-                   editComment.style.zIndex = '3000';
-                   editComment.style.background = '#fff';
-                   editComment.style.border = '1px solid #ADADAD';
-                   editComment.style.borderRadius = '10px 10px 10px 10px';
-                   editComment.style.overflow = 'hidden';
-                   editComment.style.boxShadow = '0 16px 30px -14px rgba(9,30,66,0.25), 0 0 1px rgba(9,30,66,0.31)';
-            }
+            var editComment = document.getElementById("edit-comment");
+            editComment.style.width = '80%';
+            editComment.style.left = '10%';
+            editComment.style.top = '10%';
+            editComment.style.margin = '0';
+            editComment.style.position = 'fixed';
+            editComment.style.zIndex = '3000';
+            editComment.style.border = '1px solid #FAFAFA';
+            editComment.style.borderRadius = '4px 4px 4px 4px';
+            editComment.style.overflow = 'hidden';
+            editComment.style.boxShadow = '0 16px 30px -14px rgba(9,30,66,0.25), 0 0 1px rgba(9,30,66,0.31)';
         })
     });
 
