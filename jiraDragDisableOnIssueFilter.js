8c309e0131ccdbde99099785b6d95477becd2900
Daniel Jílek <jilek.daniel@gmail.com>
2019-04-19 00:59:17 +0200

    jira drag disable on issue filter

--- jiraDragDisableOnIssueFilter.js
+++ jiraDragDisableOnIssueFilter.js
@@ -1,27 +1,40 @@
 // ==UserScript==
 // @name         Jira - drag disable on issue list
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  Disables drag option for issue rows on issue filter screen
 // @author       Daniel Jílek
 // @match        https://jira.unicorn.eu/issues/*
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
-    setTimeout(function ()
-    {
-        //clean element from event listeners
-        let oldElement = document.getElementsByClassName("ui-sortable")[0];
-        let newElement = oldElement.cloneNode(true);
-        oldElement.parentNode.replaceChild(newElement, oldElement);
+    let dragDisableTimeout = 500;
 
+    let cursorChange = function () {
         //change cursor style
         let rows = document.getElementsByClassName("issue-table-draggable");
         for (let i = 0; i < rows.length; i++) {
             rows[i].style.cursor = "default";
         }
-    }, 800);
+    };
+
+    let dragDisable = function (timeout) {
+        setTimeout(function () {
+            // clean mousedown events from website
+            function handler(e) {
+                cursorChange();
+                e.stopPropagation();
+            }
+
+            document.addEventListener("mousedown", handler, true);
+
+            cursorChange();
+
+        }, timeout);
+    };
+
+    dragDisable(dragDisableTimeout);
 })();
